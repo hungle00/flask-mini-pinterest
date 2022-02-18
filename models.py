5f1b53ed5cb22217f528491783228447cbca357b
hungle00 <hungle2048@gmail.com>
2022-02-18 17:33:30 +0700

    edit dbschema, fix authentication

--- models.py
+++ models.py
@@ -1,12 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin
-import uuid
 
 db = SQLAlchemy()
 
-class User(UserMixin, db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
+class Base(db.Model):
+    __abstract__ = True
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(),
+                              onupdate=db.func.current_timestamp())
+
+class User(Base):
     email = db.Column(db.String(100), unique=True)
     username = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(200))  # incase password hash becomes too long
@@ -15,11 +19,21 @@ class User(UserMixin, db.Model):
         return '<User {}>'.format(self.username)
 
 
-class Pin(db.Model):
-    __tablename__ = 'pins'
+class Pin(Base):
     id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(140))
-    image = db.Column(db.String(140))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    title = db.Column(db.String(140))
+    image_url = db.Column(db.String(140))
+    pin_by = db.relationship('User', backref=db.backref('pins', lazy='dynamic'))
 
     def __repr__(self):
-        return '<Pin {}>'.format(self.text)
+        return '<Pin {}>'.format(self.title)
+
+
+class Vote(Base):
+    pin_id = db.Column(db.Integer, db.ForeignKey('pin.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    pins = db.relationship('Pin', foreign_keys=[pin_id],
+                             backref=db.backref('voted_on_by', lazy='dynamic'))
+    users = db.relationship('User', foreign_keys=[user_id],
+                            backref=db.backref('voted_on', lazy='dynamic'))
