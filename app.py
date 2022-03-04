30314bea91e24966332001ee4948dbebfc7095c9
evil-never-die <longlehako@gmail.com>
2022-03-04 08:37:19 +0700

    fixed navbar and layout change

--- app.py
+++ app.py
@@ -136,6 +136,7 @@ def get_image(pin_id):
     pin_detail = this_pin.pin_detail.first()
     print(pin_detail)
     return render_template('show.html', this_pin=this_pin, pin_detail=pin_detail)
+    # return render_template('show.html', this_pin=this_pin)
 
 
 @app.route('/pins/<int:pin_id>', methods=['POST'])
